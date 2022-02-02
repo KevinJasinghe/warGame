f0c13e4e67fa80db3025655aaaab143e09ecddf9
Kevin <kevinjasinghe@gmail.com>
2022-02-02 18:53:21 -0500

    Initial Commit

---
+++ Shared/warGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  warGameApp.swift
+//  Shared
+//
+//  Created by Kevin Jasinghe on 2022-02-02.
+//
+
+import SwiftUI
+
+@main
+struct warGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
